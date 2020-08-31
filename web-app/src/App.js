c33007c25990dc193a1e25b9f341611e4e207ea6
Julian Fajardo <jafajardo37@misena.edu.co>
2020-08-31 17:04:07 -0500

    Chore: Commit de version inicial de codigo

---
+++ web-app/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          En esta pag se desarrollara el contenido para kimera...
+        </p>
+        <a
+          className="App-link"
+          href="http://kimera.com/data/multimedia/multimedia.html"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Se trabajará Incas
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
